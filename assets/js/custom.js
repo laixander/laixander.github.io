09b623eff90a6a2c1ef7bc1c66eeae81129c94ca
laixander <laixander@gmail.com>
2020-08-12 01:10:19 +0800

    Revert "Merge branch 'master' of https://github.com/laixander/laixander.github.io"
    This reverts commit 3c2f8aa48789ce26d1c0922ef2d272b8da5e7fe8, reversing
    changes made to a1f98bfedea8cd75ab4b6258c37d958704ce53d9.

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+// PRELOADER
+function preloader(){
+    $('.loader-bg').fadeToggle();
+}
+
+// SCROLL FADE ANIMATION
+AOS.init({once: true});
--- project/tg-system/icons/js/scripts.js
+++ project/tg-system/icons/js/scripts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 hljs.initHighlightingOnLoad();
 
 // copy to clipboard function
@@ -49,6 +48,3 @@ document.onkeydown = function (event) {
 		return false;  
 	}  
 }  
-=======
-hljs.initHighlightingOnLoad();const aioColors=document.querySelectorAll("code");aioColors.forEach(a=>{a.addEventListener("click",()=>{const b=window.getSelection(),c=document.createRange();c.selectNodeContents(a),b.removeAllRanges(),b.addRange(c);try{document.execCommand("copy"),b.removeAllRanges();const c=a.textContent;a.textContent="Copied!",a.classList.add("success"),setTimeout(()=>{a.textContent=c,a.classList.remove("success")},1200)}catch(a){const b=document.querySelector(".error-msg");b.classList.add("show"),setTimeout(()=>{b.classList.remove("show")},1200)}})}),document.onkeypress=function(a){if(a=a||window.event,123==a.keyCode)return!1},document.onmousedown=function(a){if(a=a||window.event,123==a.keyCode)return!1},document.onkeydown=function(a){if(a=a||window.event,123==a.keyCode)return!1};
->>>>>>> 5c1cd1f92534c37248f8394fe2d56854e86caa06
