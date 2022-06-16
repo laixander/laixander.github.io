dbfa06ffd1c92503d39461ab151c7252ad3a149b
laixander <laixander@gmail.com>
2022-06-16 11:47:09 +0800

    Enhancements

--- demo/dts/js/custom.js
+++ demo/dts/js/custom.js
@@ -9,25 +9,25 @@ $(function() {
 
     // for Scrollspy
     // Add scrollspy to <body>
-    $('body').scrollspy({target: '.navbar', offset: 50});   
-    // Add smooth scrolling on all links inside the navbar
-    $('#mainNav a, .navbar-brand').on('click', function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== '') {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-            // Store hash
-            var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-                }, 1e3, 'easeInOutExpo', function(){
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                // window.location.hash = hash;
-            });
-        }  // End if
-    });
+    // $('body').scrollspy({target: '.navbar', offset: 50});   
+    // // Add smooth scrolling on all links inside the navbar
+    // $('#mainNav a, .navbar-brand').on('click', function(event) {
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== '') {
+    //         // Prevent default anchor click behavior
+    //         event.preventDefault();
+    //         // Store hash
+    //         var hash = this.hash;
+    //         // Using jQuery's animate() method to add smooth page scroll
+    //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //             }, 1e3, 'easeInOutExpo', function(){
+    //             // Add hash (#) to URL when done scrolling (default click behavior)
+    //             // window.location.hash = hash;
+    //         });
+    //     }  // End if
+    // });
 
     // for Shrink Navbar & Scroll Top Button
     $(window).scroll(function () {
@@ -105,4 +105,3 @@ $('.slider-nav').slick({
     centerMode: true,
     focusOnSelect: true
 });
-
